c9ea3fc80ea7c3bb3a385b7dc27953c64480a4f5
Andrey <dyadickiandrey@gmail.com>
2018-01-19 21:26:50 +0700

    описаны настройки игры

--- settings.py
+++ settings.py
@@ -9,19 +9,19 @@ class Settings():
         self.bg_color = (230, 230, 230)     # цвет фона
 
         # Настройки корабля
-        self.ship_speed_factor = 1.5
+        self.ship_speed_factor = 1.5 #скорость корабля
         self.ship_limit = 3
 
         # Параметры пули
-        self.bullet_speed_factor = 3
+        self.bullet_speed_factor = 3 #скорость пули
         #self.bullet_width = 3
-        self.bullet_width = self.screen_width / 2 # для тестирования
-        self.bullet_height = 15
-        self.bullet_color = 60, 60, 60
-        self.bullets_allowed = 5
+        self.bullet_width = self.screen_width / 10 # для тестирования-ширина пули
+        self.bullet_height = 4 #толщина пули
+        self.bullet_color = 60, 60, 60 #цвет
+        self.bullets_allowed = 6
         self.bullet_inside = False
 
         # Настройки пришельцев
-        self.alien_speed_factor = 5
+        self.alien_speed_factor = 2 # скорость пришельцев
         self.fleet_drop_speed = 10  #величина снижения флота при достижении края
         self.fleet_direction = 1    #1 - движение вправо, -1 - движение влево
